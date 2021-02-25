28039554d4dca42cc2e771d967e44b2530013cb9
Botagar <botagar@gmail.com>
2021-02-25 22:31:34 +1000

    WIP setup of hello world example

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { render } from 'react-dom'
+import { hot } from 'react-hot-loader/root'
+
+const App = () => (
+  <div><p>Hello World</p></div>
+)
+
+render(React.createElement(hot(App)), document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+
+const DIST = path.resolve(__dirname, 'dist')
+const ENV = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  mode: ENV,
+  devtool: 'source-map',
+  target: 'web',
+
+  entry: {
+    main: './src/main.js'
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+
+  output: {
+    path: DIST,
+    publicPath: 'auto',
+    filename: '[name].bundle.[hash:8].js',
+    chunkFilename: '[name].bundle.[contenthash:8].js',
+    sourceMapFilename: '[file].map'
+  },
+
+  resolve: {
+    // https://webpack.js.org/configuration/resolve/
+    extensions: ['.js', '.ts', '.json', '.tsx'],
+    alias: {}
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'typescript-loader'
+        }]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ids.HashedModuleIdsPlugin(),
+    new HtmlWebpackPlugin({    
+      template: path.resolve(__dirname, './src/index.html'),     
+      filename: 'index.html',
+    }),  
+  ],
+}
