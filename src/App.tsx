408eeec97b319d77205fc924377206e86c0526bc
JGeddes <botagar@gmail.com>
2021-05-02 10:44:54 +1000

    move heightmap code to snippets dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
-import { HightMap } from './hightmap';
 
 // Let's do something like this temporarily https://www.coding-dude.com/wp/web-design/create-dynamic-backgrounds-website-using-html5-canvas/
 const App: FC = () => (
   <Container>
-    <HightMap />
     <h1>Hello World</h1>
   </Container>
 );
