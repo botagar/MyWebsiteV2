19fb38a168636db321f87790980a7ec61c250278
JGeddes <botagar@gmail.com>
2022-06-02 20:40:55 +1000

    update main to create react root instead of calling render directly

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import App from './App'
-import { NormaliseStyles } from './styling/hocs/NormaliseStyles';
+import { NormaliseStyles } from './styling/hocs/NormaliseStyles'
 
 const renderApp = () => {
-  render((
-    <NormaliseStyles>
-      <App />
-    </NormaliseStyles>
-  ), document.getElementById('app'))
+  const root = createRoot(document.getElementById('app'))
+  root.render(
+  <NormaliseStyles>
+    <App />
+  </NormaliseStyles>)
 }
 
 renderApp()
